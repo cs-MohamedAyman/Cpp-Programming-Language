e7666287755b252c1b60cae770914d4a945dd9e5
Mohamed Ayman <sw.eng.MohamedAyman@gmail.com>
2021-03-02 19:17:53 +0200

    Update Gomoku-Game-Question.cpp

--- Projects/Gomoku-Game/Gomoku-Game-Question.cpp
+++ Projects/Gomoku-Game/Gomoku-Game-Question.cpp
@@ -4,12 +4,19 @@ using namespace std;
 const int N = 15;
 const int M = 15;
 const int a_row = 5;
+const int n_players = 2;
+char marks[n_players] = {'X', 'O'};
 char grid[N][M];
 
 //This function prints the grid of Gomoku as the game progresses
 void print_grid() {
-    cout << "Player 1: B  vs  Player 2: W\n";
-    cout << "--";
+	for (int i = 0; i< n_players; i++) {
+        cout << "Player " << i+1 << ": " << marks[i] << "  ";
+        if (i < n_players-1)
+            cout << "vs  ";
+	}
+	cout << "\n";
+	cout << "--";
     for (int i = 0; i < M; cout << "---", i++);
     cout << "--\n";
     for (int i = 0; i < N; i++) {
@@ -22,7 +29,7 @@ void print_grid() {
         cout << "--\n";
     }
 }
-//This function checks if row or column or diagonal is full with same characters
+//This function checks if the game has a win state or not
 bool check_win() {
 
 }
@@ -38,12 +45,16 @@ bool check_empty(int i, int j) {
 bool check_valid_position(int i, int j) {
 
 }
-//This function sets a value to a cell
+//This function sets the given mark to the given column
 void set_cell(int i, int j, char mark) {
 
 }
-//This function clears the grid
+//This function clears the game structures
 void grid_clear() {
+
+}
+//This function reads a valid position input
+void read_input(int &i, int &j) {
 
 }
 //MAIN FUNCTION
@@ -51,39 +62,34 @@ void play_game() {
     cout << "Gomoku Game!\n";
     cout << "Welcome...\n";
     cout << "============================\n";
-    bool player = 0;
+    int player = 0;
     while (true) {
         //Prints the grid
         print_grid();
-        //Set mark value based on the player
-        char mark = (player == 0? 'B' : 'W');
         //Read an input from the player
-        cout << "Player " << mark << '\n';
+        cout << "Player " << marks[player] << " is playing now\n";
         int i, j;
-        cout << "Enter the row index and column index: ";
-        cin >> i >> j;
-        while (!check_valid_position(i, j) || !check_empty(i, j)) {
-            cout << "Enter a valid row index and a valid column index: ";
-            cin >> i >> j;
-        }
+		read_input(i, j);
         //Set the input position with the mark
-        set_cell(i, j, mark);
+        set_cell(i, j, marks[player]);
         //Check if the state of the grid has a win state
         if (check_win()) {
             //Prints the grid
             print_grid();
-            cout << "Congrats, Player " << mark << " is won!\n";
+            //Announcement of the final statement
+            cout << "Congrats, Player " << marks[player] << " is won!\n";
             break;
         }
         //Check if the state of the grid has a tie state
-        if (check_tie(mark)) {
+        if (check_tie(marks[player])) {
             //Prints the grid
             print_grid();
+            //Announcement of the final statement
             cout << "Woah! That's a tie!\n";
             break;
         }
         //Player number changes after each turn
-        player = 1 - player;
+        player = (player + 1) % n_players;
     }
 }
 int main() {
