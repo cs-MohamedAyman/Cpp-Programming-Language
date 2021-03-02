46469f1a46b7d0f8cf6e74a44a3ddfa22a896143
Mohamed Ayman <sw.eng.MohamedAyman@gmail.com>
2021-03-02 10:44:38 +0200

    Update Tic-Tac-Toe-Game-Question.cpp

--- Projects/Tic-Tac-Toe-Game/Tic-Tac-Toe-Game-Question.cpp
+++ Projects/Tic-Tac-Toe-Game/Tic-Tac-Toe-Game-Question.cpp
@@ -2,11 +2,12 @@
 using namespace std;
 
 const int N = 3;
+const char marks[2] = {'X', 'O'};
 char grid[N][N];
 
 //This function prints the grid of Tic-Tac-Toe as the game progresses
 void print_grid() {
-    cout << "Player 1: X  vs  Player 2: O\n";
+    cout << "Player 1: " << marks[0] << "  vs  Player 2: " << marks[1] << "\n";
     cout << "--";
     for (int i = 0; i < N; cout << "---", i++);
     cout << "--\n";
@@ -20,36 +21,12 @@ void print_grid() {
         cout << "--\n";
     }
 }
-//This function checks if row or column or diagonal is full with same characters
+//This function checks if the game has a win state or not
 bool check_win() {
-    //If row is full with same characters, 
-	//the game is over and the player with that character has won
-
-    //If column is full with same characters, 
-	//the game is over and the player with that character has won
-
-    //If diagonal 1 is full with same characters, 
-	//the game is over and the player with that character has won
-
-    //If diagonal 2 is full with same characters, 
-	//the game is over and the player with that character has won
-
-    //Otherwise, there isn't a win state in the game, 
-    //if all cases above not reached	
 
 }
-//This function checks if row or column or diagonal is full with same characters
+//This function checks if the game has a tie state or not
 bool check_tie(char mark) {
-    //If row a single type of characters
-
-    //If column a single type of characters
-
-    //If diagonal a single type of characters
-
-    //If diagonal a single type of characters
-
-    //Otherwise, there isn't a win state in the game, 
-    //if all cases above not reached	
 
 }
 //This function checks if given cell is empty or not 
@@ -60,12 +37,16 @@ bool check_empty(int i, int j) {
 bool check_valid_position(int i, int j) {
 
 }
-//This function sets a value to a cell
+//This function sets the given mark to the given cell
 void set_cell(int i, int j, char mark) {
 
 }
-//This function clears the grid
+//This function clears the game structures
 void grid_clear() {
+
+}
+//This function reads a valid position input
+void read_input(int &i, int &j) {
 
 }
 //MAIN FUNCTION
@@ -77,30 +58,25 @@ void play_game() {
     while (true) {
         //Prints the grid
         print_grid();
-        //Set mark value based on the player
-        char mark = (player == 0? 'X' : 'O');
-        //Read an input from the player
-        cout << "Player " << mark << '\n';
+        //Read an input position from the player
+        cout << "Player " << marks[player] << " is playing now\n";
         int i, j;
-        cout << "Enter the row index and column index: ";
-        cin >> i >> j;
-        while (!check_valid_position(i, j) || !check_empty(i, j)) {
-            cout << "Enter a valid row index and a valid column index: ";
-            cin >> i >> j;
-        }
-        //Set the input position with the mark
-        set_cell(i, j, mark);
-        //Check if the state of the grid has a win state
+		read_input(i, j);
+        //Set the player mark in the input position
+        set_cell(i, j, marks[player]);
+        //Check if the grid has a win state
         if (check_win()) {
             //Prints the grid
             print_grid();
-            cout << "Congrats, Player " << mark << " is won!\n";
+			//Announcement of the final statement
+            cout << "Congrats, Player " << marks[player] << " is won!\n";
             break;
         }
-        //Check if the state of the grid has a tie state
-        if (check_tie(mark)) {
+        //Check if the grid has a tie state
+        if (check_tie(marks[player])) {
             //Prints the grid
             print_grid();
+			//Announcement of the final statement
             cout << "Woah! That's a tie!\n";
             break;
         }
