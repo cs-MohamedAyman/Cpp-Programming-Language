61835007d07c8da5f8c828b1ac299c3e4622be12
Mohamed Ayman <sw.eng.MohamedAyman@gmail.com>
2021-03-02 10:43:36 +0200

    Update Tic-Tac-Toe-Game-Answer.cpp

--- Projects/Tic-Tac-Toe-Game/Tic-Tac-Toe-Game-Answer.cpp
+++ Projects/Tic-Tac-Toe-Game/Tic-Tac-Toe-Game-Answer.cpp
@@ -2,11 +2,12 @@
 using namespace std;
 
 const int N = 3;
+const char marks[2] = {'X', 'O'};
 char grid[N][N];
 
 //This function prints the grid of Tic-Tac-Toe as the game progresses
 void print_grid() {
-    cout << "Player 1: X  vs  Player 2: O\n";
+    cout << "Player 1: " << marks[0] << "  vs  Player 2: " << marks[1] << "\n";
     cout << "--";
     for (int i = 0; i < N; cout << "---", i++);
     cout << "--\n";
@@ -20,11 +21,11 @@ void print_grid() {
         cout << "--\n";
     }
 }
-//This function checks if row or column or diagonal is full with same characters
+//This function checks if the game has a win state or not
 bool check_win() {
     char s;
     int cnt;
-    //If row is full with same characters, 
+    //Check if there is a row with full with same characters, 
 	//the game is over and the player with that character has won
     for (int i = 0; i < N; i++) {
 		s = grid[i][0];
@@ -35,7 +36,7 @@ bool check_win() {
 		if (cnt == N && s != '.')
 			return true;
     }
-    //If column is full with same characters, 
+    //Check if there is a column with full with same characters, 
 	//the game is over and the player with that character has won
     for (int i = 0; i < N; i++) {
 		s = grid[0][i];
@@ -46,7 +47,7 @@ bool check_win() {
 		if (cnt == N && s != '.')
 			return true;
     }
-    //If diagonal 1 is full with same characters, 
+    //Check if the left diagonal is full with same characters, 
 	//the game is over and the player with that character has won
     s = grid[0][0];
 	cnt = 0;
@@ -55,7 +56,7 @@ bool check_win() {
 		    cnt ++;
 	if (cnt == N && s != '.')
 		return true;
-    //If diagonal 2 is full with same characters, 
+    //Check if the right diagonal is full with same characters, 
 	//the game is over and the player with that character has won
     s = grid[N-1][0];
 	cnt = 0;
@@ -65,14 +66,14 @@ bool check_win() {
 	if (cnt == N && s != '.')
 		return true;
     //Otherwise, there isn't a win state in the game, 
-    //if all cases above not reached	
+    //if all cases above not reached
     return false;
 }
-//This function checks if row or column or diagonal is full with same characters
+//This function checks if the game has a tie state or not
 bool check_tie(char mark) {
     char s;
     int cnt;
-    //If row a single type of characters
+    //Check if there is a row with a single type of characters
     for (int i = 0; i < N; i++) {
 		s = mark;
 		cnt = 0;
@@ -82,7 +83,7 @@ bool check_tie(char mark) {
 		if (cnt == 0)
 			return false;
     }
-    //If column a single type of characters
+    //Check if there is a column with a single type of characters
     for (int i = 0; i < N; i++) {
 		s = mark;
 		cnt = 0;
@@ -92,7 +93,7 @@ bool check_tie(char mark) {
 		if (cnt == 0)
 			return false;
     }
-    //If diagonal a single type of characters
+    //Check if the left diagonal is a single type of characters
 	s = mark;
 	cnt = 0;
     for (int i = 0; i < N; i++)
@@ -100,7 +101,7 @@ bool check_tie(char mark) {
 		    cnt ++;
 	if (cnt == 0)
 		return false;
-    //If diagonal a single type of characters
+    //Check if the right diagonal is a single type of characters
 	s = mark;
 	cnt = 0;
     for (int i = 0; i < N; i++)
@@ -109,7 +110,7 @@ bool check_tie(char mark) {
 	if (cnt == 0)
 		return false;
     //Otherwise, there isn't a win state in the game, 
-    //if all cases above not reached	
+    //if all cases above not reached
 	return true;
 }
 //This function checks if given cell is empty or not 
@@ -120,15 +121,24 @@ bool check_empty(int i, int j) {
 bool check_valid_position(int i, int j) {
 	return 0 <= i && i < N && 0 <= j && j < N;
 }
-//This function sets a value to a cell
+//This function sets the given mark to the given cell
 void set_cell(int i, int j, char mark) {
 	grid[i][j] = mark;
 }
-//This function clears the grid
+//This function clears the game structures
 void grid_clear() {
     for (int i = 0; i < N; i++)
         for (int j = 0; j < N; j++)
             grid[i][j] = '.';
+}
+//This function reads a valid position input
+void read_input(int &i, int &j) {
+	cout << "Enter the row index and column index: ";
+	cin >> i >> j;
+    while (!check_valid_position(i, j) || !check_empty(i, j)) {
+		cout << "Enter a valid row index and a valid column index: ";
+		cin >> i >> j;
+	}
 }
 //MAIN FUNCTION
 void play_game() {
@@ -139,30 +149,25 @@ void play_game() {
     while (true) {
         //Prints the grid
         print_grid();
-        //Set mark value based on the player
-        char mark = (player == 0? 'X' : 'O');
-        //Read an input from the player
-        cout << "Player " << mark << '\n';
+        //Read an input position from the player
+        cout << "Player " << marks[player] << " is playing now\n";
         int i, j;
-        cout << "Enter the row index and column index: ";
-        cin >> i >> j;
-        while (!check_valid_position(i, j) || !check_empty(i, j)) {
-            cout << "Enter a valid row index and a valid column index: ";
-            cin >> i >> j;
-        }
-        //Set the input position with the mark
-        set_cell(i, j, mark);
-        //Check if the state of the grid has a win state
+		read_input(i, j);
+        //Set the player mark in the input position
+        set_cell(i, j, marks[player]);
+        //Check if the grid has a win state
         if (check_win()) {
             //Prints the grid
             print_grid();
-            cout << "Congrats, Player " << mark << " is won!\n";
+			//Announcement of the final statement
+            cout << "Congrats, Player " << marks[player] << " is won!\n";
             break;
         }
-        //Check if the state of the grid has a tie state
-        if (check_tie(mark)) {
+        //Check if the grid has a tie state
+        if (check_tie(marks[player])) {
             //Prints the grid
             print_grid();
+			//Announcement of the final statement
             cout << "Woah! That's a tie!\n";
             break;
         }
