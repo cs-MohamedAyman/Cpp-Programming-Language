c286ee1ab1909efedcd1d20c8bd5bca864f8acf2
Mohamed Ayman <sw.eng.MohamedAyman@gmail.com>
2021-04-08 16:48:06 +0200

    Update Gomoku-Game-Answer.cpp

--- Projects/Gomoku-Game/Gomoku-Game-Answer.cpp
+++ Projects/Gomoku-Game/Gomoku-Game-Answer.cpp
@@ -5,7 +5,7 @@ const int N = 9;
 const int M = 9;
 const int a_row = 5;
 const int n_players = 2;
-char marks[n_players] = {'X', 'O'};
+char marks[n_players] = {'B', 'W'};
 char grid[N][M];
 
 //This function prints the grid of Gomoku as the game progresses
